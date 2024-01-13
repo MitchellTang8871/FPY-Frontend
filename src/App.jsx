07e63a431fa567a01e03ae6eacacdd71ec35806f
Mitchell Tang <129018940+MitchellTang8871@users.noreply.github.com>
2024-01-13 15:55:33 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios"
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Register from './pages/Register';
 
 function App() {
-  useEffect(() => {
-    axios.defaults.baseURL = `http://localhost:8000`
-    axios.defaults.timeout = 15000
-}, []);
-  return (
-    <Routes>
-      <Route path="/" element={<Register/>} />
-      <Route index element={<Register />} />
-    </Routes>
-  )
+    useEffect(() => {
+        axios.defaults.baseURL = `http://localhost:8000`
+        axios.defaults.timeout = 15000
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index path="/" element={<Register/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
